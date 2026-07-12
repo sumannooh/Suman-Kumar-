8555f0fcbb5bd4bb1ee02418859b76171eea6dc8
sumannooh <aiartsuman@gmail.com>
2026-07-12 11:15:58 +0530

    Add login function for user authentication

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function login() {
+
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "suman" && pass === "22314") {
+        document.getElementById("message").innerHTML =
+        "✅ Welcome Suman!";
+    } else {
+        document.getElementById("message").innerHTML =
+        "❌ Wrong Username or Password";
+    }
+
+}
 function updateClock() {
     const now = new Date();
     document.getElementById("clock").innerHTML =
